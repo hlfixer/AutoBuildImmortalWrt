8fe0580f53d4d952a2941e78a121f06489bdb0b8
hlfixer <33725595+hlfixer@users.noreply.github.com>
2025-11-21 08:32:09 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -54,7 +54,7 @@ PACKAGES="$PACKAGES open-vm-tools"
 # PACKAGES="$PACKAGES luci-app-argon-config"
 # PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 #24.10
-# PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 # PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 # PACKAGES="$PACKAGES luci-app-passwall"
 # PACKAGES="$PACKAGES luci-app-passwall2"
